6733176e55b12d1d34560a7a59b6f7d70a788d91
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-09 11:54:49 +0530

    Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Gnric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("maximum Number is:");
+            int output = Number.MaximumIntNum(15, 25, 35);
+            Console.WriteLine(output);
+        }
+    }
+}
